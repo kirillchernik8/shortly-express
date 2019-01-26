434dba02edaa4bdcc390eaa760f7d58f96028f35
kirillchernik8 <kirillchernik8@icloud.com>
2019-01-26 10:56:56 -0500

    Add the user model

--- app/models/user.js
+++ app/models/user.js
@@ -1,15 +1,20 @@
 var db = require('../config');
-var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
+var bcrypt = Promise.promisify(require('bcrypt-nodejs').bcrypt);
 
 
 
 var User = db.Model.extend({
-    tableName: 'users',
-    hasTimestamps: true,
-    initialize: () => {
-      
-      Promise.promisify()
-    }
+  tableName: 'users',
+  hasTimestamps: true,
+  initialize: () => {
+    this.on('creating', function (model, attrs, options) {
+      bcrypt.hash(model.get('password'), null).then((hash) => {
+        model.set('password', hash);
+      }).catch((err) => {
+        console.log(err);
+      });
+    });
+  }
 });
 module.exports = User;
